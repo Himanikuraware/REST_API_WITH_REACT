bcbc09a2ecf2f30072c635b4ccdadfe02e94c1b2
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-15 16:43:01 +0530

    Added Validation on BE.

--- Server/controllers/feed.js
+++ Server/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -15,6 +17,14 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      // 422 status code :- Validation failed
+      message: "Validation failed, Please enter correct data",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
   const content = req.body.content;
   res.status(201).json({
