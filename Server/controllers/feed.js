3032310dfc1855a7935dca3df6f5ca4552b026f8
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-15 17:38:38 +0530

    fetched single post.

--- Server/controllers/feed.js
+++ Server/controllers/feed.js
@@ -3,19 +3,16 @@ const { validationResult } = require("express-validator");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: Math.random().toString(),
-        title: "The First post",
-        content: "This is the first post",
-        creator: {
-          name: "Himani",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({ posts: posts });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
@@ -39,6 +36,27 @@ exports.createPost = (req, res, next) => {
       res.status(201).json({
         message: "Post created successfully",
         post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Couldn't find the post.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({
+        post: post,
       });
     })
     .catch((err) => {
