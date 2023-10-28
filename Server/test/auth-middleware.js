7b05fe10b7506fa29805dfcb1ad6ec2a045de549
Himanikuraware <kurawarehimani26@gmail.com>
2023-10-28 14:18:21 +0530

    Used stubs for testing external dependencies.

--- Server/test/auth-middleware.js
+++ Server/test/auth-middleware.js
@@ -1,12 +1,39 @@
 const expect = require("chai").expect;
+const jwt = require("jsonwebtoken");
 
 const authMiddleware = require("../middleware/is-auth");
 
-it("should throw an error if no authorization header is present", () => {
-  const req = {
-    get: (headerName) => {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw("Not authenticated.");
+describe("Auth middleware", () => {
+  it("should throw an error if no authorization header is present", () => {
+    const req = {
+      get: (headerName) => {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      "Not authenticated."
+    );
+  });
+
+  it("should throw an error if the token cannot be verified", () => {
+    const req = {
+      get: (headerName) => {
+        return xyz;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId after decoding the token", () => {
+    const req = {
+      get: (headerName) => {
+        return "xyjlkhklhckjhdkljchlcjhlhclhlckhdhklz";
+      },
+    };
+    jwt.verify = function () {
+      return { userId: "xyz" };
+    };
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+  });
 });
